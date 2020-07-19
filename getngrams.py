afa4592bdcf1a1da37218a05a9870985de300f87
Maya Bose <47787632+boseHere@users.noreply.github.com>
2020-07-18 21:15:53 -0700

    Update getngrams.py

--- getngrams.py
+++ getngrams.py
@@ -1,120 +1,208 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*
+
 from ast import literal_eval
-from pandas import DataFrame  # http://github.com/pydata/pandas
+import pandas as pd
 import re
-import requests               # http://github.com/kennethreitz/requests
+import html.parser
+import functools
+import requests              
 import subprocess
 import sys
-
-corpora = dict(eng_us_2012=17, eng_us_2009=5, eng_gb_2012=18, eng_gb_2009=6,
-               chi_sim_2012=23, chi_sim_2009=11, eng_2012=15, eng_2009=0,
-               eng_fiction_2012=16, eng_fiction_2009=4, eng_1m_2009=1,
-               fre_2012=19, fre_2009=7, ger_2012=20, ger_2009=8, heb_2012=24,
-               heb_2009=9, spa_2012=21, spa_2009=10, rus_2012=25, rus_2009=12,
-               ita_2012=22)
+import os
+import argparse
 
 
-def getNgrams(query, corpus, startYear, endYear, smoothing, caseInsensitive):
-    params = dict(content=query, year_start=startYear, year_end=endYear,
-                  corpus=corpora[corpus], smoothing=smoothing,
-                  case_insensitive=caseInsensitive)
-    if params['case_insensitive'] is False:
-        params.pop('case_insensitive')
-    if '?' in params['content']:
-        params['content'] = params['content'].replace('?', '*')
-    if '@' in params['content']:
-        params['content'] = params['content'].replace('@', '=>')
-    req = requests.get('http://books.google.com/ngrams/graph', params=params)
-    res = re.findall('var data = (.*?);\\n', req.text)
-    if res:
-        data = {qry['ngram']: qry['timeseries']
-                for qry in literal_eval(res[0])}
-        df = DataFrame(data)
-        df.insert(0, 'year', list(range(startYear, endYear + 1)))
-    else:
-        df = DataFrame()
-    return req.url, params['content'], df
+def get_args():
+    """
+    This function uses the argparse library to parse command line arguments.
+
+    Returns:
+        args (argparse.Namespace): An argparse object. Elements of the object 
+        can be accessed by their option name as attributes.
+    """
+    parser = argparse.ArgumentParser(description=
+    "This program takes a list of words, or a file containing a list of words,\
+ and queries the Google NGram API for the usage frequency of these words, by\
+ year, within a desginated time-frame. Unless '--noSave' is indicated, data is\
+ saved to a file called 'google_Ngram_output.csv'. A more detailed description\
+ of various arguments can be found at https://books.google.com/ngrams/info")
+
+    parser.add_argument("Query", type = str, nargs="+", help="List of words,\
+ or CSV or TXT file of words to be queried")
+
+    parser.add_argument("-c", "--corpus", default="eng_2019", type = str, 
+        help ="Shorthand name for corpus of words to be queried. Available\
+ corpora can be read about at https://books.google.com/ngrams/info. Default is\
+ English corpus, 2019 update")
+
+    parser.add_argument("-s", "--startYear", default=1800, type=int,
+        help = "A year: beginning of time range to be queried. Default is 1800")
+
+    parser.add_argument("-e", "--endYear", default = 2000, type = int,
+        help = "A year: last year of time range to be included in query.\
+ Default is 2000")
+
+    parser.add_argument("-sm", "--smoothing", default = 0, type = int,
+        help = "The degree to which data points are averaged between years. A\
+ smoothing of 0 indicates completely raw data. Default is 0.")
+
+    parser.add_argument("-ci", "--caseInsensitive", action="store_true", 
+        help = "Consider upper- and lower-case versions of the words")
+
+    parser.add_argument("-a", "--allData", action="store_true")
+
+    parser.add_argument("-n", "--noSave", action="store_true", 
+        help = "Use to prevent data from being saved to external file")
+
+    parser.add_argument("-q", "--quiet", action="store_true",
+        help="Use to prevent program from printing to STD OUT")
+
+    parser.add_argument("-o", "--outputDir", default = "./", type = str,
+        help = "Directory to save output file to")
+
+    parser.add_argument("-p", "--plot", action="store_true", 
+        help = "Create plot of data")
+
+    args = parser.parse_args()
+    args.Query = "".join(args.Query).split(",")
+
+    return args
 
 
-def runQuery(argumentString):
-    arguments = argumentString.split()
-    query = ' '.join([arg for arg in arguments if not arg.startswith('-')])
-    if '?' in query:
-        query = query.replace('?', '*')
-    if '@' in query:
-        query = query.replace('@', '=>')
-    params = [arg for arg in arguments if arg.startswith('-')]
-    corpus, startYear, endYear, smoothing = 'eng_2012', 1800, 2000, 3
-    printHelp, caseInsensitive, allData = False, False, False
-    toSave, toPrint, toPlot = True, True, False
+def get_words(args):
+    """
+    This function takes in a filename of words seperated by either commas,
+    newlines, or both, and returns these words in a set.
 
-    # parsing the query parameters
-    for param in params:
-        if '-nosave' in param:
-            toSave = False
-        elif '-noprint' in param:
-            toPrint = False
-        elif '-plot' in param:
-            toPlot = True
-        elif '-corpus' in param:
-            corpus = param.split('=')[1].strip()
-        elif '-startYear' in param:
-            startYear = int(param.split('=')[1])
-        elif '-endYear' in param:
-            endYear = int(param.split('=')[1])
-        elif '-smoothing' in param:
-            smoothing = int(param.split('=')[1])
-        elif '-caseInsensitive' in param:
-            caseInsensitive = True
-        elif '-alldata' in param:
-            allData = True
-        elif '-help' in param:
-            printHelp = True
+    Args:
+        args (argparse.Namespace): An argparse object. Elements of the object 
+        can be accessed by their option name as attributes.
+
+    Returns:
+        list of strings : A list of unique words.
+    """
+    words_list = []
+    filename = ""
+    # Access words input through commandline, or save name of file given in 
+    # command line
+    for element in args.Query:
+        if element.endswith(".txt") or element.endswith(".csv"):
+            filename = element
         else:
-            print(('Did not recognize the following argument: %s' % param))
-    if printHelp:
-        print('See README file.')
-    else:
-        if '*' in query and caseInsensitive is True:
-            caseInsensitive = False
-            notifyUser = True
-            warningMessage = "*NOTE: Wildcard and case-insensitive " + \
-                             "searches can't be combined, so the " + \
-                             "case-insensitive option was ignored."
-        elif '_INF' in query and caseInsensitive is True:
-            caseInsensitive = False
-            notifyUser = True
-            warningMessage = "*NOTE: Inflected form and case-insensitive " + \
-                             "searches can't be combined, so the " + \
-                             "case-insensitive option was ignored."
-        else:
-            notifyUser = False
-        url, urlquery, df = getNgrams(query, corpus, startYear, endYear,
-                                      smoothing, caseInsensitive)
-        if not allData:
-            if caseInsensitive is True:
-                for col in df.columns:
-                    if col.count('(All)') == 1:
-                        df[col.replace(' (All)', '')] = df.pop(col)
-                    elif col.count(':chi_') == 1 or corpus.startswith('chi_'):
-                        pass
-                    elif col.count(':ger_') == 1 or corpus.startswith('ger_'):
-                        pass
-                    elif col.count(':heb_') == 1 or corpus.startswith('heb_'):
-                        pass
-                    elif col.count('(All)') == 0 and col != 'year':
-                        if col not in urlquery.split(','):
-                            df.pop(col)
-            if '_INF' in query:
-                for col in df.columns:
-                    if '_INF' in col:
+            words_list.append(element)
+
+    # collect words from file input from command line
+    if len(filename) != 0:
+        with open(filename) as in_file:
+            for line in in_file:
+                line = line.strip()
+                words = line.split(",")
+                for word in words:
+                    words_list.append(word)
+
+    # clean data
+    for i in range(len(words_list)):
+        word = words_list[i]
+        word = word.strip()
+        if '?' in word:
+            word = word.replace('?', '*')
+        if '@' in word:
+            word = word.replace('@', '=>')
+        words_list[i] = word
+    
+    return list(set(words_list))
+
+
+def collect_data(args, words_list):
+    """This function runs queries to Google NGram, 9 words at a time. This 
+    grouping step is necessary because of restrictions set by the Google NGram 
+    viewer itself.
+
+    Args:
+        args (argparse.Namespace): An argparse object. Elements of the object 
+        can be accessed by their option name as attributes.
+        words_list (list of strings): A list of unique words.
+
+    Returns:
+        dfs (list of DataFrames): A list of pandas DataFrame objects
+    """
+    dfs = []
+    for i in range(0, len(words_list) - (len(words_list) % 9), 9):
+        new_query = ",".join(words_list[i: i + 9]) # create string of 9 or 
+                                        # fewer words
+        new_df = runQuery(args, new_query)
+        dfs.append(new_df)
+
+    # sentinel run
+    last_df = runQuery(args, ",".join(words_list[len(words_list) 
+                                                    - len(words_list) % 9:]))
+    dfs.append(last_df)
+
+    return dfs
+
+
+def runQuery(args, query):
+    """This function processes parameters and submits queries to the getNgrams 
+    function. Warning messages are printed for conflicting parameters. Results 
+    are printed to standard output. Columns in the dataframe are cleaned, and 
+    the dataframe is returned.
+
+    Args:
+        args (argparse.Namespace): An argparse object. Elements of the object 
+        can be accessed by their option name as attributes.
+        query (string): Contains words to be queried
+
+    Returns:
+        df (pandas.core.frame.DataFrame): A pandas DataFrame object containing
+        word frequency data.
+    """
+    html_parser = html.parser.HTMLParser()
+
+    # Error handling
+    if "*" in query and args.caseInsensitive:
+        args.caseInsensitive = False
+        print("*NOTE: Wildcard and case-insensitive searches can't be combined,\
+ so the case-insensitive option was ignored.")
+
+    elif "_INF" in query and args.caseInsensitive:
+        args.caseInsensitive = False
+        print("*NOTE: Inflected form and case-insensitive searches can't be\
+ combined, so the case-insensitive option was ignored.")
+
+    # get data
+    url, urlquery, df = getNgrams(query, args.corpus, args.startYear,
+        args.endYear, args.smoothing, args.caseInsensitive)
+    url = html_parser.unescape(url).strip()
+    urlquery = html_parser.unescape(urlquery).strip()
+    list_urlquery = urlquery.split(',')
+
+    # process data
+    for i in range(len(list_urlquery)):
+        list_urlquery[i] = list_urlquery[i].strip()
+        list_urlquery[i] = "".join(list_urlquery[i].split())
+
+    if not args.allData:
+        if args.caseInsensitive:
+            for col in df.columns:
+                col_un = str(html_parser.unescape(col).strip())
+                if col.count('(All)') == 1:
+                    df[col.replace(' (All)', '')] = df.pop(col)
+                elif col.count('(All)') == 0 and col != 'year':
+                    if "".join(col_un.split()) not in list_urlquery:
                         df.pop(col)
-            if '*' in query:
-                for col in df.columns:
-                    if '*' in col:
-                        df.pop(col)
-        if toPrint:
+        
+        if '_INF' in query:
+            for col in df.columns:
+                if '_INF' in col:
+                    df.pop(col)
+        if '*' in query:
+            for col in df.columns:
+                if '*' in col:
+                    df.pop(col)
+
+        # print output
+        if not args.quiet:
             print((','.join(df.columns.tolist())))
             for row in df.iterrows():
                 try:
@@ -122,40 +210,103 @@ def runQuery(argumentString):
                            ','.join(['%.12f' % s for s in row[1].values[1:]])))
                 except:
                     print((','.join([str(s) for s in row[1].values])))
-        queries = ''.join(urlquery.replace(',', '_').split())
-        if '*' in queries:
-            queries = queries.replace('*', 'WILDCARD')
-        if caseInsensitive is True:
-            word_case = 'caseInsensitive'
-        else:
-            word_case = 'caseSensitive'
-        filename = '%s-%s-%d-%d-%d-%s.csv' % (queries, corpus, startYear,
-                                              endYear, smoothing, word_case)
-        if toSave:
+
+        if not args.noSave:
             for col in df.columns:
                 if '&gt;' in col:
                     df[col.replace('&gt;', '>')] = df.pop(col)
-            df.to_csv(filename, index=False)
-            print(('Data saved to %s' % filename))
-        if toPlot:
-            try:
-                subprocess.call(['python', 'xkcd.py', filename])
-            except:
-                if not toSave:
-                    print(('Currently, if you want to create a plot you ' +
-                           'must also save the data. Rerun your query, ' +
-                           'removing the -nosave option.'))
-                else:
-                    print(('Plotting Failed: %s' % filename))
-        if notifyUser:
-            print(warningMessage)
 
-if __name__ == '__main__':
-    argumentString = ' '.join(sys.argv[1:])
-    if argumentString == '':
-        argumentString = eval(input('Enter query (or -help):'))
+    return df
+
+
+def getNgrams(query, corpus_name, startYear, endYear, smoothing, caseInsensitive):
+    """
+    This function queries data from the Google Ngram database, using 
+    user-defined parameters. The results of this query are returned as a 
+    pandas DataFrame.
+
+    Args:
+        query (string): Contains words to be queried
+        corpus_name (string): Shorthand name for corpus of words to be queried
+        startYear (int): A year: beginning of time range to be queried
+        endYear (int): A year: last year of time range to be included in query
+        smoothing (int): The degree to which data points are averaged between 
+                            years
+        caseInsensitive (bool): Whether or not to consider upper- and 
+                                    lower-case versions of the words
+
+    Returns:
+        req.url (string): The url representing the query to Google Ngram
+        params['content'] (string) : A list of the words being queried
+        df (pandas.core.frame.DataFrame): A pandas DataFrame object containing
+        word frequency data. 
+    """
+
+    # Define parameters
+    params = dict(content=query, year_start=startYear, year_end=endYear,
+    corpus=corpus_name, smoothing=smoothing,
+    case_insensitive=caseInsensitive)
+
+    if params['case_insensitive'] is False:
+        params.pop('case_insensitive')
+    if '?' in params['content']:
+        params['content'] = params['content'].replace('?', '*')
+    if '@' in params['content']:
+        params['content'] = params['content'].replace('@', '=>')
+
+    req = requests.get('http://books.google.com/ngrams/graph', params=params)
+    res = re.findall('ngrams.data = (.*?);\\n', req.text)
+    
+    if res:
+        data = {qry['ngram']: qry['timeseries']
+                for qry in literal_eval(res[0])}
+        df = pd.DataFrame(data)
+        df.insert(0, 'year', list(range(startYear, endYear + 1)))
     else:
+        df = pd.DataFrame()
+
+    return req.url, params['content'], df
+
+
+def output_df(dfs, args):
+    """
+    This function combines all of the created DataFrame objects into a single 
+    DataFrame. The DataFrame is output to 'google_Ngram_output.csv'. A plot 
+    is drawn if the user has included the '--plot' flag. 
+
+    Args:
+        dfs (list of DataFrames): A list of pandas DataFrame objects to be 
+        combined.
+        args (argparse.Namespace): An argparse object. Elements of the object 
+        can be accessed by their option name as attributes.
+    """
+    html_parser = html.parser.HTMLParser()
+    filename = args.outputDir + "google_Ngram_output.csv"
+    bigdf = functools.reduce(lambda x, y: pd.merge(x, y, on = "year",
+                                                            how="left"), dfs)
+    new_columns = []
+    for col in bigdf.columns:
+        new_columns.append(html_parser.unescape(col))
+    for i in range(len(new_columns)):
+        new_columns[i] = new_columns[i].replace("_x", "")
+        new_columns[i] = new_columns[i].replace("_y", "")
+    bigdf.columns = new_columns
+    bigdf.to_csv(filename, index=False, header=True, encoding='utf-8-sig')
+
+    if args.plot:
         try:
-            runQuery(argumentString)
+            subprocess.call(['python', 'xkcd.py', filename])
         except:
-            print('An error occurred.')
+            if args.noSave:
+                print(('Currently, if you want to create a plot you ' +
+                        'must also save the data. Rerun your query, ' +
+                        'removing the -nosave option.'))
+            else:
+                print(("Plotting Failed: " + filename))
+
+
+if __name__ == "__main__":
+    args = get_args()
+    words_list = get_words(args)
+    dfs = collect_data(args, words_list)
+    output_df(dfs, args)
